20a9619a741f1636aa1709841c1f3ad088060085
vitoriareges <vitoriareges@hotmail.com>
2020-04-22 19:32:32 +0000

    Animal class

--- src/Animals.java
+++ src/Animals.java
@@ -1,23 +1,30 @@
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Random;
 
-import javax.sound.sampled.SourceDataLine;
 
 public class Animals {
-    int i;
+    
     Random r = new Random(); // implementing random function
-    String[] animaltype = { "cat", "dog", "ferret", "rabbit" };// array with types of animals
-    String[] names = { "Chico", "Bambi", "Guido", "Mimi", "Black", "White", "Suzy", "Willy", "Suzy", "Hopper",
+    private String[] animaltype = { "cat", "dog", "ferret", "rabbit" };// array with types of animals
+    private String[] names = { "Chico", "Bambi", "Guido", "Mimi", "Black", "White", "Suzy", "Willy", "Suzy", "Hopper",
             "Marble" };
-    String[] condition = { "Arthritis", "Cancer", "Dental Disease", "Diabetes", "Distemper", "Ear Mites", "Epilepsy", "Bloat", "Worm", "Rabies","Parvovirus" };
+    private String[] condition = { "Arthritis", "Cancer", "Dental Disease", "Diabetes", "Distemper", "Ear Mites", "Epilepsy", "Bloat", "Worm", "Rabies","Parvovirus" };
 
-    ArrayList<String> animalname = new ArrayList<String>();// creates an arrray list of names for animals
-    for(i=0; i<names.lenght;i++){
-    animalname.add(names[i]);//populating list with array of names
+
+
+public Collection<String> listNames(){
+    ArrayList<String> animalname = new ArrayList<String>(); // creates an arrray list of names for animals
+    for(int i=0; i<this.names.length; i++) {
+        animalname.add(names[i]);// populating list with array of names
     }
+    return animalname;
+}
 
-    ArrayList<String> medcond = new ArrayList<String>();
-    for(i=0; i<condition.lenght;i++){
-    medcond.add(condition[i]);//populating list with array of conditions
-    }// creates an array list of medical conditions for animals
+public Collection<String> listConditions(){
+    ArrayList<String> medcond = new ArrayList<String>(); // creates an array list of medical conditions for animals
+    for(int i=0; i<this.condition.length;i++){
+    medcond.add(this.condition[i]);//populating list with array of conditions
+    }
+    return medcond;
 }}
